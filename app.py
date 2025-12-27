a2a13b01664b60bd06eb996c6b9078068a438a46
escobarjme <90652531+escobarjme@users.noreply.github.com>
2025-12-27 08:56:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-import os
+mport os
+import requests
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("TEST ENV")
+# =====================
+# Variables de entorno (Railway)
+# =====================
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SITE_ID = os.getenv("SITE_ID", "MLA")
 
-st.write("CLIENT_ID:", os.getenv("CLIENT_ID"))
-st.write("CLIENT_SECRET:", "OK" if os.getenv("CLIENT_SECRET") else None)
-st.write("REDIRECT_URI:", os.getenv("REDIRECT_URI"))
+# Debug visual
+st.write("SITE_ID:", SITE_ID)
+
+if not CLIENT_ID or not CLIENT_SECRET or not REDIRECT_URI:
+    st.error("❌ Variables de entorno faltantes en Railway")
+    st.stop()
