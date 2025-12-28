94bfed12401a44c923e220014984257b5ce4484b
escobarjme <90652531+escobarjme@users.noreply.github.com>
2025-12-28 14:04:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ AUTH_URL = "https://auth.mercadolibre.com.ar/authorization"
 TOKEN_URL = f"{API_BASE}/oauth/token"
 
 CATEGORIES = {
-    "Televisores": "MLA1002",
+    "Repuestos Autos/Camionetas": "MLA1747",
     "Celulares": "MLA1055",
     "Notebooks": "MLA1652",
     "Zapatillas": "MLA109027",
