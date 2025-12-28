0b9583dc350c7b4c1fa88691190b851bdc6db824
escobarjme <90652531+escobarjme@users.noreply.github.com>
2025-12-28 14:26:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ TOKEN_URL = f"{API_BASE}/oauth/token"
 
 CATEGORIES = {
     "Repuestos Autos/Camionetas": "MLA5725",
-    "Motor": "MLA3172",
+    "Motor": "MLA73292",
     "Notebooks": "MLA1652",
     "Zapatillas": "MLA109027",
 }
