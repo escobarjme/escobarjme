cde535743efa53820419e13d6432bac644171022
escobarjme <90652531+escobarjme@users.noreply.github.com>
2025-12-30 21:37:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ TOKEN_URL = f"{API_BASE}/oauth/token"
 CATEGORIES = {
     "Accesorios para Vehículos (Gral)": "MLA5725",
     "Repuestos Autos y Camionetas": "MLA3483",
-    "Baterías": "MLA2220",
+    "Inyeccion": "MLA437355",
     "Neumáticos": "MLA22195",
     "Lubricantes y Fluidos": "MLA456111",
     "Notebooks": "MLA1652",
